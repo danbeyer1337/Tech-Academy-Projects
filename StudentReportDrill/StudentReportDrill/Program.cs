41619ce8e10544149e61cd0666d3dc9c3cca5926
Dan Beyer <dan.beyer1337@gmail.com>
2018-06-08 16:50:22 -0700

    Added the rest of the questions and working on question 3 still

--- StudentReportDrill/StudentReportDrill/Program.cs
+++ StudentReportDrill/StudentReportDrill/Program.cs
@@ -20,22 +20,40 @@ namespace StudentReportDrill
             Console.WriteLine("You are on course:" + yourCourse);
 
             //Question 2
-            Console.WriteLine("What page number?");
+            Console.WriteLine("What page number are you on?");
             string pageNumber = Console.ReadLine();
             Console.WriteLine("You are on page number " + pageNumber);
 
             //Question 3
-            Console.WriteLine("Do you need help with anything? Please answer true or false.");
-            bool needHelp = true;
-            if (needHelp)
+            Console.WriteLine("Do you need any help with anything? Please answer \"true\" or \"false\".");
+            bool x = true;
+            string needHelp = Console.ReadLine();
+            Console.WriteLine(Convert.ToBoolean(needHelp));
+            if (x == true)
             {
-                Console.WriteLine("You do not need help.");
+                Console.WriteLine("We can help! What do you need help with?");
             }
             else
             {
-                Console.WriteLine("What do you need help with?");
+                Console.WriteLine("Let us know if you need anything!");
             }
-            
+            Console.ReadLine();
+            //Question 4
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specfics.");
+            string feedBack = Console.ReadLine();
+            Console.WriteLine("Thank you for your feedback.");
+
+            //Question 5
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedBack1 = Console.ReadLine();
+            Console.WriteLine("Thank you for your feedback.");
+
+            //Question 6
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            Console.WriteLine("Good job on studying" + hours);
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
 
 
             Console.ReadLine();
