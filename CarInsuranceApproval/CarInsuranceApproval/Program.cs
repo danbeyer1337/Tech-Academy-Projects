65d54a2472e66c30afa96172739e9051f493c72c
Dan Beyer <dan.beyer1337@gmail.com>
2018-06-28 21:32:37 -0700

    Uploading drill pg41

---
+++ CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int userAge;
+            userAge = Convert.ToInt32(Console.ReadLine());
+            //bool oldEnough = (userAge >= 15);
+            //Console.WriteLine(oldEnough);
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool x;
+            string duiAnswer;
+            duiAnswer = Convert.ToString(Console.ReadLine());
+            x = Convert.ToBoolean(duiAnswer);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int userTickets;
+            userTickets = Convert.ToInt32(Console.ReadLine());
+            //bool howManyTickets = (userTickets < 3);
+            //Console.WriteLine(howManyTickets);
+
+             
+            //Console.WriteLine("Qualified?");
+            //bool userApproved = (userAge >= 15) && (duiAnswer = false) && (userTickets < 3);
+            //Console.WriteLine(userApproved);
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ShippingQuoteDrillpg41/ShippingQuoteDrillpg41/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuoteDrillpg41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight: ");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else if (packageWeight <= 50){
+                Console.WriteLine("Please enter the package height: ");
+                int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package width: ");
+                int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length: ");
+                int packageLength = Convert.ToInt32(Console.ReadLine());
+                if (packageWidth + packageLength + packageHeight > 50)
+                {
+                    Console.WriteLine("Package is too big to shipped Package Express. Good Bye.");
+                }
+                else
+                {
+                    int customerQuote = (packageWidth * packageWidth * packageLength * packageHeight / 100);
+                    Console.WriteLine("Your estimated total for shipping this package is: " + customerQuote +);
+                }
+                
+                   
+                
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
